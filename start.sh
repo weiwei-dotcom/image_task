17c92caebe6a60dd6fc0d0bb7972d28aadc98243
weiwei-dotcom <728841808@qq.com>
2024-11-17 19:11:50 +0800

    根目录下添加了start.sh脚本直接启动normal_yolo_detect.launch.py

---
+++ start.sh
@@ -0,0 +1,32 @@
+source ./install/setup.bash
+
+if [ $# == 0 ]; then
+    ros2 launch task_launch normal_yolo_detect.launch.py
+
+elif [ $# == 1 ];then
+    if [ "$1" == 'detect' ]; then
+        ros2 launch task_launch normal_yolo_detect.launch.py
+        # TODO: 
+    fi
+
+elif [ $# == 2 ]; then
+
+    if [ "$1" == 'detect' ]; then
+        if [ "$2" == 'normal' ]; then
+            ros2 launch task_launch normal_yolo_detect.launch.py
+        fi
+    fi
+
+elif [ $# == 3 ]; then
+    if [ "$1" == 'detect' ]; then
+        if [ "$2" == 'normal' ]; then
+            if [ "$3" == 'yolo' ]; then
+                ros2 launch task_launch normal_yolo_detect.launch.py
+            fi
+        fi
+    fi
+
+else
+    echo "please input bootargs!!"
+fi
+
