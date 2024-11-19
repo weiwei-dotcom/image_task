ec928d6ab22ec4d2dda027dc178517ec0e697180
weiwei-dotcom <728841808@qq.com>
2024-11-19 21:51:47 +0800

    增加了获取目标二维位置点的服务接口,编写完成获取目标点二维位置服务端

--- src/detect/detect/test.py
+++ src/detect/detect/test.py
@@ -3,7 +3,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from rclpy.node import Node
 from ultralytics import YOLO
-from interface.srv import TarState
+from interface.srv import ObjState
 from rclpy.parameter import Parameter
 import cv2
 import yaml
@@ -18,26 +18,10 @@ class YoloDetector(Node):
         self.bridge = CvBridge()
         self.model = YOLO(self.config["model_path"])
         self.img_sub = self.create_subscription(Image, 'image', self.ImgCallBack, 10)
-        self.get_tar_cli = self.create_service(TarState, 'get_tar_state', self.GetTarStateCallback)
         self.color = [[]]
         for i in range(1000):
             self.color.append([random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)])
         
-    def GetTarStateCallback(self, request, response):
-        response.success = False
-        max_area = 0.0
-        for result in self.results:
-            for box in result.boxes:
-                if request.name is not result.names[int(box.cls[0])]:
-                    break
-                temp_area = box.xywhn[0][2] * box.xywhn[0][3]
-                if temp_area < self.config["min_area"]:
-                    break
-                response.success = True
-                if max_area < temp_area:
-                    max_area = temp_area
-                    response.x = (box.xyxy[0][0] + box.xyxy[0][2]) / 2
-                    response.y = (box.xyxy[0][1] + box.xyxy[0][3]) / 2
         
     def InitParams(self):
         with open("/home/wl/Documents/image_task/src/detect/config/yolo_detect.yaml", mode="r", encoding="utf8") as file:
@@ -48,26 +32,23 @@ class YoloDetector(Node):
         self.declare_parameter("confidence", 0.5)
         self.declare_parameter("font_size", 2)
         self.declare_parameter("thickness", 3)
-        self.declare_parameter("min_area", 0.1)
         self.set_parameters([
             Parameter('model_path', Parameter.Type.STRING, self.config_yaml["yolo_detect"]["ros__parameters"]["model_path"]),
             Parameter('fps', Parameter.Type.INTEGER, self.config_yaml["yolo_detect"]["ros__parameters"]["fps"]),
             Parameter('confidence', Parameter.Type.DOUBLE, self.config_yaml["yolo_detect"]["ros__parameters"]["confidence"]),
             Parameter('font_size', Parameter.Type.INTEGER, self.config_yaml["yolo_detect"]["ros__parameters"]["font_size"]),
-            Parameter('min_area', Parameter.Type.DOUBLE, self.config_yaml["yolo_detect"]["ros__parameters"]["min_area"]),
             Parameter('thickness', Parameter.Type.INTEGER, self.config_yaml["yolo_detect"]["ros__parameters"]["thickness"])])
         self.config["model_path"] = self.get_parameter("model_path").get_parameter_value().string_value
         self.config["fps"] = self.get_parameter("fps").get_parameter_value().integer_value
         self.config["confidence"] = self.get_parameter("confidence").get_parameter_value().double_value
-        self.config["min_area"] = self.get_parameter("min_area").get_parameter_value().double_value
         self.config["font_size"] = self.get_parameter("font_size").get_parameter_value().integer_value
         self.config["thickness"] = self.get_parameter("thickness").get_parameter_value().integer_value
         
         
     def ImgCallBack(self, img_msg):
         cv_img = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding='bgr8')
-        self.results = self.model.predict(cv_img, conf=self.config["confidence"])
-        for result in self.results:
+        results = self.model.predict(cv_img, conf=self.config["confidence"])
+        for result in results:
             for box in result.boxes:
                 cv2.rectangle(cv_img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])), (int(box.xyxy[0][2]), int(box.xyxy[0][3])), self.color[int(box.cls[0])], self.config["thickness"])
                 cv2.putText(cv_img, f"{result.names[int(box.cls[0])]}", (int(box.xyxy[0][0]), int(box.xyxy[0][1]+25)), cv2.FONT_HERSHEY_PLAIN, self.config["font_size"], self.color[int(box.cls[0])], self.config["thickness"])
