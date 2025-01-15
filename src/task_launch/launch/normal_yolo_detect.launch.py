2619d6bfee16691c25ba41d02fda29a07b5f259e
weiwei-dotcom <728841808@qq.com>
2025-01-15 14:15:32 +0800

    .在camera以及detect功能包中使用cmakelist将config文件安装到install对应功能包的share/pkg-name/config目录下面,在launch文件中使用get_package_share_directory找到功能包路径来读取参数文件路径传入parameters中

--- src/task_launch/launch/normal_yolo_detect.launch.py
+++ src/task_launch/launch/normal_yolo_detect.launch.py
@@ -1,16 +1,27 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+
+    normal_camera_pkg_dir = get_package_share_directory("camera")
+    normal_camera_file_name = "normal_camera.yaml"
+    normal_camera_file_path = os.path.join(normal_camera_pkg_dir, "config", normal_camera_file_name)
+
+    yolo_pkg_dir = get_package_share_directory("detect")
+    yolo_file_name = "yolo_detect.yaml"
+    yolo_file_path = os.path.join(yolo_pkg_dir, "config", yolo_file_name)
+    
     normal_camera_node = Node(
         package="camera",
         executable='normal',
-        parameters = ["/home/wl/Documents/image_task/src/camera/config/normal_camera.yaml"]
+        parameters = [normal_camera_file_path]
     )
     yolo_detect_node = Node(
         package="detect",
         executable='yolo',
-        parameters = ["/home/wl/Documents/image_task/src/detect/config/yolo_detect.yaml"]
+        parameters = [yolo_file_path]
     )
     launch_description = LaunchDescription(
         [normal_camera_node, yolo_detect_node]
--- src/task_launch/launch/realsense_yolo_detect.launch.py
+++ src/task_launch/launch/realsense_yolo_detect.launch.py
@@ -1,16 +1,28 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
 
 def generate_launch_description():
+
+    realsense_pkg_dir = get_package_share_directory("camera")
+    realsense_file_name = "realsense_camera.yaml"
+    realsense_file_path = os.path.join(realsense_pkg_dir, "config", realsense_file_name)
+
+    yolo_pkg_dir = get_package_share_directory("detect")
+    yolo_file_name = "yolo_detect.yaml"
+    yolo_file_path = os.path.join(yolo_pkg_dir, "config", yolo_file_name)
+    
     realsense_camera_node = Node(
         package="camera",
         executable='realsense',
-        parameters = ["/home/wl/Documents/image_task/src/camera/config/realsense_camera.yaml"]
+        parameters = [realsense_file_path]
     )
     yolo_detect_node = Node(
         package="detect",
         executable='yolo',
-        parameters = ["/home/wl/Documents/image_task/src/detect/config/yolo_detect.yaml"]
+        parameters = [yolo_file_path]
     )
     launch_description = LaunchDescription(
         [realsense_camera_node, yolo_detect_node]
